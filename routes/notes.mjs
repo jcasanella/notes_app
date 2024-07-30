ab1c579a9d54599ef659e49b3ca727d48605b264
Jordi Casanella <jordikiter@hotmail.es>
2024-07-30 23:23:52 +0100

    Create wrapper function to remove try/catch

--- routes/notes.mjs
+++ routes/notes.mjs
@@ -1,5 +1,6 @@
 import { default as express } from 'express';
 import { notesStore } from '../app.mjs';
+import { runAsyncWrapper } from './routessupport.mjs';
 
 export const router = express.Router();
 
@@ -14,38 +15,36 @@ router.get('/add', (req, res, next) => {
 });
 
 // Save Note
-router.post('/save', async (req, res, next) => {
-    try {
-        let note;
+router.post(
+    '/save',
+    runAsyncWrapper(async (req, res, next) => {
         const { noteKey, title, body } = req.body;
         if (req.body.doCreate === 'create') {
-            note = await notesStore.create(noteKey, title, body);
+            await notesStore.create(noteKey, title, body);
         } else {
-            note = await notesStore.update(noteKey, title, body);
+            await notesStore.update(noteKey, title, body);
         }
         res.redirect('/notes/view?key=' + noteKey);
-    } catch (error) {
-        next(error);
-    }
-});
+    })
+);
 
 // Read Note
-router.get('/view', async (req, res, next) => {
-    try {
+router.get(
+    '/view',
+    runAsyncWrapper(async (req, res, next) => {
         let note = await notesStore.read(req.query.key);
         res.render('noteView', {
             title: note ? note.title : '',
             noteKey: req.query.key,
             note,
         });
-    } catch (error) {
-        next(error);
-    }
-});
+    })
+);
 
 // Edit note (update)
-router.get('/edit', async (req, res, next) => {
-    try {
+router.get(
+    '/edit',
+    runAsyncWrapper(async (req, res, next) => {
         const note = await notesStore.read(req.query.key);
         res.render('noteEdit', {
             title: note ? 'Edit ' + note.title : 'Add a Note',
@@ -53,31 +52,27 @@ router.get('/edit', async (req, res, next) => {
             noteKey: req.query.key,
             note,
         });
-    } catch (error) {
-        next(error);
-    }
-});
+    })
+);
 
 // Ask to delete note (destroy)
-router.get('/destroy', async (req, res, next) => {
-    try {
+router.get(
+    '/destroy',
+    runAsyncWrapper(async (req, res, next) => {
         const note = await notesStore.read(req.query.key);
         res.render('noteDestroy', {
             title: note ? note.title : '',
             noteKey: req.query.key,
             note,
         });
-    } catch (error) {
-        next(error);
-    }
-});
+    })
+);
 
 // Really destroy note (Destroy)
-router.post('/destroy/confirm', async (req, res, next) => {
-    try {
+router.post(
+    '/destroy/confirm',
+    runAsyncWrapper(async (req, res, next) => {
         await notesStore.destroy(req.body.noteKey);
         res.redirect('/');
-    } catch (error) {
-        next(error);
-    }
-});
+    })
+);
---
+++ routes/routessupport.mjs
@@ -0,0 +1,5 @@
+export function runAsyncWrapper(callback) {
+    return function (req, res, next) {
+        callback(req, res, next).catch(next);
+    };
+}
